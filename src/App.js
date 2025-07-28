90e14216dc2e376632b58dd22c97b01a237766ec
Huy Nguyen <nghuy164@gmail.com>
2025-07-28 19:40:00 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import { Header } from './components/Header';
+import { NetCalories } from './components/NetCalories';
 function App() {
   return (
     <div>
       <Header />
+      <div className='container'>
+        <NetCalories />
+      </div>
     </div>
   );
 }
---
+++ src/components/NetCalories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const NetCalories = () => {
+  return (
+    <>
+    <h4>Net Calories</h4>
+    <h1 id='net-calories'>0.00</h1>
+    </>
+  )
+}
+
